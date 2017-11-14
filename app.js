374c6ded63a14991cf0dd8346b307fa4a3737f9a
Noe Alcantar <noealcantar@MacBook-Pro-de-Noe.local>
2017-11-13 23:16:19 -0500

    Création de templates

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use('/public', express.static('public'));
+
+//EJS
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.get("/movies", (req, res) => {
+   res.send("Bientôt de films ici");
+});
+
+app.get('/movies/add', (req, res) => {
+   res.send("Prochainement un formulaire ici");
+});
+
+app.get('/movies/:id', (req, res) => {
+   const id = req.params.id;
+   res.send(`Film numéro ${id}`);
+});
+
+app.get('/', (req, res) => {
+   //res.send("Hello <b>WORLD</b>!!");
+    res.render('index');
+});
+
+app.listen(PORT, () => {
+   console.log(`Listening on port ${PORT}`);
+});
