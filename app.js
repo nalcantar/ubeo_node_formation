915b74b85bb7af75b3f8968d52b112392f345f4d
Noe Alcantar <noealcantar@MacBook-Pro-de-Noe.local>
2017-11-14 22:18:21 -0500

    module 5

--- app.js
+++ app.js
@@ -1,38 +1,73 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const upload = multer();
 const PORT = 3000;
 
-app.use('/public', express.static('public'));
+let frenchMovies = [];
+
+app.use( '/public', express.static('public') );
+//app.use( bodyParser.urlencoded( { extended: false } ) );
 
 //EJS
-app.set('views', './views');
-app.set('view engine', 'ejs');
+app.set( 'views', './views' );
+app.set( 'view engine', 'ejs' );
 
-app.get("/movies", (req, res) => {
-   //res.send("Bientôt de films ici");
-    res.render('movies');
+app.get( "/movies", (req, res) => {
+
+    const title = "Films de plusieurs années";
+
+    frenchMovies = [
+        { title : "Le fabuleux destin d'Amélie Poulin", year : 2001 },
+        { title : "Thor", year : 2015 },
+        { title : "Bon cop bad cop", year : 1999 },
+        { title : "les aventures de Richie", year : 1923 }
+    ];
+    res.render('movies', { movies : frenchMovies, title : title } );
 });
 
-app.get("/movie-details", (req, res) => {
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+/*app.post( '/movies', urlencodedParser, (req, res) => {
+    console.log( req.body.movietitle );
+    const newMovie = { title : req.body.movietitle, year : req.body.movieyear };
+    frenchMovies = [ ...frenchMovies, newMovie ];
+    console.log( frenchMovies );
+    res.sendStatus(201);
+});*/
+
+app.post('/movies', upload.fields([])  , (req, res) => {
+    if ( !req.body ) {
+        return res.sendStatus( 500 );
+    } else {
+        const formData = req.body;
+        console.log( 'formData : ', formData );
+        const newMovie = { title : req.body.movietitle, year : req.body.movieyear };
+        frenchMovies = [ ...frenchMovies, newMovie ];
+        res.sendStatus(201);
+    }
+});
+
+app.get( "/movie-details", (req, res) => {
     //res.send("Bientôt de films ici");
-    res.render('movies-details');
+    res.render('movies-details' );
 });
 
-app.get('/movies/add', (req, res) => {
+app.get( '/movies/add', (req, res) => {
    res.send("Prochainement un formulaire ici");
 });
 
-app.get('/movies/:id', (req, res) => {
+app.get( '/movies/:id', (req, res) => {
    const id = req.params.id;
-   res.send(`Film numéro ${id}`);
-    res.render('movies-details');
+   res.render( 'movies-details', { movieid: id } );
 });
 
-app.get('/', (req, res) => {
+app.get( '/', (req, res) => {
    //res.send("Hello <b>WORLD</b>!!");
-    res.render('index');
+    res.render( 'index' );
 });
 
-app.listen(PORT, () => {
-   console.log(`Listening on port ${PORT}`);
+app.listen( PORT, () => {
+   console.log( `Listening on port ${PORT}` );
 });
